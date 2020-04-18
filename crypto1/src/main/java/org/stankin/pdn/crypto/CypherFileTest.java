bb25b0dbceab9a5e5820e569174ad8c3dda203e9
Ustinov Ivan <ustinovis@kamatech.ru>
2020-04-19 01:59:55 +0300

    Тестирование шифрования 1

---
+++ crypto1/src/main/java/org/stankin/pdn/crypto/CypherFileTest.java
@@ -0,0 +1,54 @@
+package org.stankin.pdn.crypto;
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
+import java.security.Security;
+import java.util.Arrays;
+
+public class CypherFileTest {
+    public static void main(String[] args) throws Exception {
+        Security.addProvider(new BouncyCastleProvider());
+
+        KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+        keyGenerator.init(256);
+        SecretKey secretKey = keyGenerator.generateKey();
+        Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+        System.out.println("AES key length = " + secretKey.getEncoded().length);
+
+        File defaultFile = new File("/Users/ivanustinov/Downloads/0000.txt");
+
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+        File encryptedFile = new File("/Users/ivanustinov/Downloads/test/enc" + defaultFile.getName());
+        assert encryptedFile.delete();
+
+        encryptDecryptFile(cipher, encryptedFile, Files.readAllBytes(defaultFile.toPath()));
+
+        File decryptedFile = new File("/Users/ivanustinov/Downloads/test/dec" + defaultFile.getName());
+        assert decryptedFile.delete();
+        cipher.init(Cipher.DECRYPT_MODE, secretKey);
+
+        encryptDecryptFile(cipher, decryptedFile, Files.readAllBytes(encryptedFile.toPath()));
+    }
+
+    private static void encryptDecryptFile(Cipher cipher, File destFile, byte[] srcFileByteArr) throws Exception {
+        Files.write(destFile.toPath(), new byte[0], StandardOpenOption.CREATE_NEW);
+        int totalLength = srcFileByteArr.length;
+        int byteLeft = totalLength;
+        int currentPosition = 0;
+
+        byte[] arr;
+        while (byteLeft >= 128) {
+            arr = Arrays.copyOfRange(srcFileByteArr, currentPosition, currentPosition += 64);
+            Files.write(destFile.toPath(), cipher.update(arr), StandardOpenOption.APPEND);
+            byteLeft -= 128;
+        }
+        arr = Arrays.copyOfRange(srcFileByteArr, currentPosition, totalLength);
+        Files.write(destFile.toPath(), cipher.doFinal(arr), StandardOpenOption.APPEND);
+    }
+}
---
+++ crypto1/src/main/java/org/stankin/pdn/crypto/CypherKeyTest.java
@@ -0,0 +1,41 @@
+package org.stankin.pdn.crypto;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.SecureRandom;
+import java.util.Arrays;
+import java.util.Base64;
+
+public class CypherKeyTest {
+    public static void main(String[] args) throws Exception {
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+        keyPairGenerator.initialize(2048);
+        KeyPair keyPair = keyPairGenerator.generateKeyPair();
+        Cipher cipher = Cipher.getInstance("RSA/ECB/OAEPWithSHA-256AndMGF1Padding");
+
+        KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+        keyGenerator.init(256);
+        SecretKey aesSecretKey = keyGenerator.generateKey();
+
+        cipher.init(Cipher.PUBLIC_KEY, keyPair.getPublic());
+        byte[] encryptedKey = cipher.doFinal(aesSecretKey.getEncoded());
+
+        cipher.init(Cipher.PRIVATE_KEY, keyPair.getPrivate());
+        byte[] decryptedKey = cipher.doFinal(encryptedKey);
+
+        SecretKey originalKey = new SecretKeySpec(decryptedKey , 0, decryptedKey.length, "AES");
+        System.out.println("Orig: " + Arrays.toString(aesSecretKey.getEncoded()));
+        System.out.println("Enc : " + Arrays.toString(encryptedKey));
+        System.out.println("Orig: " + Arrays.toString(originalKey.getEncoded()));
+
+        assert aesSecretKey.getEncoded() == originalKey.getEncoded();
+        System.out.println("Success");
+    }
+
+
+}
